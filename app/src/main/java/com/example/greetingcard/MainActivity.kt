3ea58462c7eb4036a4af29b3a4b34021bba87ccc
Robert Gitei <robert.gitei@yopeso.com>
2022-07-13 15:24:44 +0300

    Changes greeting message

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -31,13 +31,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "sup, $name here")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GreetingCardTheme {
-        Greeting("Android")
+        Greeting("Robert")
     }
 }
