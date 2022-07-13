31c661f36bbe575ca9ff2f985e029b3dd1822ecd
Robert Gitei <robert.gitei@yopeso.com>
2022-07-13 15:29:05 +0300

    Adds padding to the text

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +36,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Surface(color = Color.Cyan) {
-        Text(text = "sup, $name here")
+        Text(text = "sup, $name here", modifier = Modifier.padding(24.dp))
     }
 }
 
